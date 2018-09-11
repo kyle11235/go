7893cec20e34ef64574b52250db11530f63d22cb
kyle11235 <aizuguo_@126.com>
2018-09-11 22:49:12 +0800

    update proxyjs

--- proxy.js
+++ proxy.js
@@ -2,30 +2,33 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
-var desHost = 'http://localhost:3001';
-
 app.use('/', function (req, res) {
-	var url = req.url;
-	var newURL = desHost + url;
-	console.log(url);
+	var url = req.query.url;
+	console.log('real url=' + req.query.url);
+	
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	res.setHeader('Content-Type', 'application/json;charset=UTF-8');
-	// skip OPTIONS
+
 	if (req.method === 'OPTIONS') {
 		console.log('\nOPTIONS method.');
 		res.send('');
 	} else {
 		var resProxy = res;
 		var options = {
-			url: newURL,
-			// proxy: 'http://cn-proxy.cn.oracle.com:80'
+			url: url
 		};
-
 		req.pipe(request(options)).pipe(resProxy);
 	}
 
 });
-app.listen(process.env.PORT || 8888);
+console.log('running at 8080...');
+app.listen(process.env.PORT || 8080);
+
+
+// npm install express
+// npm install request
+// node proxy.js
+// http://localhost:8080?url=realUrl
