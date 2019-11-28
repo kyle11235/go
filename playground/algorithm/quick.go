708bfc3e4e0a645a8784657ad20ee604f9f965b5
kyle11235 <kyle@example.com>
2019-11-28 14:09:43 +0800

    clean

---
+++ playground/algorithm/quick.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+// two way linked list quick sort
+func quickSort(values []int, left, right int) {
+	temp := values[left]
+	p := left
+	i, j := left, right
+
+	for i <= j {
+		for j >= p && values[j] >= temp {
+			j--
+		}
+		if j >= p {
+			values[p] = values[j]
+			p = j
+		}
+
+		for i <= p && values[i] <= temp {
+			i++
+		}
+		if i <= p {
+			values[p] = values[i]
+			p = i
+		}
+	}
+	values[p] = temp
+	if p-left > 1 {
+		quickSort(values, left, p-1)
+	}
+	if right-p > 1 {
+		quickSort(values, p+1, right)
+	}
+}
+
+func main() {
+
+	arr := []int{1, 5, 3, 6, 2}
+	quickSort(arr, 0, len(arr)-1)
+	fmt.Println(arr)
+}
