8d749a1f9365472d9620f4c84d65cf674e2e1b6e
kyle11235 <kyle@example.com>
2019-11-28 13:04:46 +0800

    clean

---
+++ playground/algorithm/bitmap.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Bitmap struct {
+	words  []uint64
+	length int
+}
+
+func New() *Bitmap {
+	return &Bitmap{}
+}
+func (bitmap *Bitmap) Has(num int) bool {
+	word, bit := num/64, uint(num%64)
+	// 1<<bit = move 1 to left bit times
+	return word < len(bitmap.words) && (bitmap.words[word]&(1<<bit)) != 0
+}
+
+func (bitmap *Bitmap) Add(num int) {
+	word, bit := num/64, uint(num%64)
+	for word >= len(bitmap.words) {
+		bitmap.words = append(bitmap.words, 0)
+	}
+
+	if bitmap.words[word]&(1<<bit) == 0 {
+		bitmap.words[word] |= 1 << bit
+		bitmap.length++
+	}
+}
+
+func (bitmap *Bitmap) Len() int {
+	return bitmap.length
+}
+
+func (bitmap *Bitmap) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, v := range bitmap.words {
+		if v == 0 {
+			continue
+		}
+		for j := uint(0); j < 64; j++ {
+			if v&(1<<j) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*uint(i)+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	fmt.Fprintf(&buf, "\nLength: %d", bitmap.length)
+	return buf.String()
+}
+
+func main() {
+
+	bitmap := New()
+	bitmap.Add(123)
+	bitmap.Add(456)
+	fmt.Println(bitmap.Has(123))
+	fmt.Println(bitmap.String())
+
+	// true
+	// {123 456}
+	// Length: 2
+
+}
